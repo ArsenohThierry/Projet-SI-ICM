626eefebed7ab62a45e5a97c9bc5a641ff2a3a27
noraarandrianarivelo <mahatsangyaaron.randrianarivelo@outlook.com>
2026-05-09 15:40:49 +0300

    creation ObjectifModel

---
+++ app/Models/ObjectifModel.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class ObjectifModel extends Model
+{
+    protected $table = 'objectif';
+    protected $primaryKey = 'id';
+
+    protected $allowedFields = [
+        'libelle'
+    ];
+
+}
